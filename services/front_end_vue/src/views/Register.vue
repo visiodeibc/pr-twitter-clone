5d915c21466d0458ab83a9373fb2c3944985ee18
visiodeibc <ljh95714@gmail.com>
2022-04-11 23:46:29 +0800

    center aligned

--- services/front_end_vue/src/views/Register.vue
+++ services/front_end_vue/src/views/Register.vue
@@ -1,19 +1,21 @@
 <template>
   <section>
     <form @submit.prevent="submit">
-      <div class="mb-3">
+      <div class="mb-3 w-50 mx-auto">
         <label for="username" class="form-label">Username:</label>
         <input type="text" name="username" v-model="user.username" class="form-control" />
       </div>
-      <div class="mb-3">
+      <div class="mb-3 w-50 mx-auto">
         <label for="full_name" class="form-label">Full Name:</label>
         <input type="text" name="full_name" v-model="user.full_name" class="form-control" />
       </div>
-      <div class="mb-3">
+      <div class="mb-3 w-50 mx-auto">
         <label for="password" class="form-label">Password:</label>
         <input type="password" name="password" v-model="user.password" class="form-control" />
       </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <div class="col text-center">
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </div>
     </form>
   </section>
 </template>

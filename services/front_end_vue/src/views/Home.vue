13749989f339f5a97adf615ca9270e573104a4df
visiodeibc <ljh95714@gmail.com>
2022-04-12 23:26:47 +0800

    v-else apply

--- services/front_end_vue/src/views/Home.vue
+++ services/front_end_vue/src/views/Home.vue
@@ -1,44 +1,60 @@
 <template>
   <section>
     <div v-if="tweets.length">
-        <div v-for="tweet in tweets" :key="tweet.id" class="tweets">
-          <div class="card" style="width: 18rem;">
-            <div class="card-body">
-              <ul>
-                <li><strong>Tweet Title:</strong> {{ tweet.title }}</li>
-                <li><strong>Content:</strong> {{ tweet.content }}</li>
-                <li><strong>Author:</strong> {{ tweet.author.username }}</li>
-                <li><router-link :to="{name: 'Tweet', params:{id: tweet.id}}">View</router-link></li>
-              </ul>
-            </div>
+      <div v-for="tweet in tweets" :key="tweet.id" class="tweets">
+        <div class="card" style="width: 18rem">
+          <div class="card-body">
+            <ul>
+              <li><strong>Tweet Title:</strong> {{ tweet.title }}</li>
+              <li><strong>Content:</strong> {{ tweet.content }}</li>
+              <li><strong>Author:</strong> {{ tweet.author.username }}</li>
+              <li>
+                <router-link :to="{ name: 'Tweet', params: { id: tweet.id } }"
+                  >View</router-link
+                >
+              </li>
+            </ul>
           </div>
-          <br/>
         </div>
+        <br />
       </div>
+    </div>
+    <div v-else>
+      <p>This site is built with FastAPI and Vue.</p>
+      <div v-if="isLoggedIn" id="logout">
+        <p id="logout">
+          Click <a href="/dashboard">here</a> to view all tweetss.
+        </p>
+      </div>
+      <p v-else>
+        <span><a href="/register">Register</a></span>
+        <span> or </span>
+        <span><a href="/login">Log In</a></span>
+      </p>
+    </div>
   </section>
 </template>
 
-
 <script>
-import { mapGetters, mapActions } from 'vuex';
+import { mapGetters, mapActions } from "vuex";
 export default {
-  name: 'Dashboard',
+  name: "Dashboard",
   data() {
     return {
       form: {
-        title: '',
-        content: '',
+        title: "",
+        content: "",
       },
     };
   },
-  created: function() {
-    return this.$store.dispatch('getTweets');
+  created: function () {
+    return this.$store.dispatch("getTweets");
   },
   computed: {
-    ...mapGetters({ tweets: 'stateTweets'}),
+    ...mapGetters({ tweets: "stateTweets" }),
   },
   methods: {
-    ...mapActions(['createTweet']),
+    ...mapActions(["createTweet"]),
     async submit() {
       await this.createTweet(this.form);
     },

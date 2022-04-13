af61db48933a94dbcace1da4e80585a6b599c6b6
visiodeibc <ljh95714@gmail.com>
2022-04-13 21:08:30 +0800

    center align list

--- services/front_end_vue/src/views/Home.vue
+++ services/front_end_vue/src/views/Home.vue
@@ -2,18 +2,23 @@
   <section>
     <div v-if="tweets.length">
       <div v-for="tweet in tweets" :key="tweet.id" class="tweets">
-        <div class="card" style="width: 18rem">
-          <div class="card-body">
-            <ul>
-              <li><strong>Tweet Title:</strong> {{ tweet.title }}</li>
-              <li><strong>Content:</strong> {{ tweet.content }}</li>
-              <li><strong>Author:</strong> {{ tweet.author.username }}</li>
-              <li>
-                <router-link :to="{ name: 'Tweet', params: { id: tweet.id } }"
-                  >View</router-link
-                >
-              </li>
-            </ul>
+        <div class="container">
+          <div class="row justify-content-md-center">
+            <div class="card" style="width: 18rem">
+              <div class="card-body">
+                <ul>
+                  <li><strong>Tweet Title:</strong> {{ tweet.title }}</li>
+                  <li><strong>Content:</strong> {{ tweet.content }}</li>
+                  <li><strong>Author:</strong> {{ tweet.author.username }}</li>
+                  <li>
+                    <router-link
+                      :to="{ name: 'Tweet', params: { id: tweet.id } }"
+                      >View</router-link
+                    >
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
         <br />
